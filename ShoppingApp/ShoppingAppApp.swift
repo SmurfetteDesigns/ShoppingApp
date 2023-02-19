901385af1a067221ad5b98f74546c5c07fbfffb0
Amanda Doige <SmurfetteDesigns@gmail.com>
2023-02-19 12:59:00 -0500

    Initial commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Smurfette1 on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
